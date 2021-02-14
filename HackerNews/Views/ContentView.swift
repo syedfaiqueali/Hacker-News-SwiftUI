17723faf0979eeb9672cae3bf8482ee5fe8188ad
Faiq Ali <faiqueali017@gmail.com>
2021-02-14 20:02:57 +0500

    Added DetailView when click on the list

--- HackerNews/Views/ContentView.swift
+++ HackerNews/Views/ContentView.swift
@@ -14,8 +14,12 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             List(networkManager.posts){ post in
-                Text(String(post.points))
-                Text(post.title)
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack{
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
             }
             .navigationBarTitle("Hacker News")
         }
---
+++ HackerNews/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  HackerNews
+//
+//  Created by Faiq on 14/02/2021.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
